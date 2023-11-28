267c2b8846c82cdb1fe5da11fc771f4eb3bb5da3
Rose van Leeuwen <i499833@fhict.nl>
2023-11-28 09:23:35 +0100

    fixed hydration error

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,10 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <header>
       <link rel="icon" type="image/x-icon" href="https://git.digitalindividuals.com/uploads/-/system/appearance/header_logo/1/div_logo_2.png" />
-        <script src='src="/socket.io/socket.io.js"' defer/>
-      </header>
       <body className={inter.className}>{children}</body>
     </html>
   )
