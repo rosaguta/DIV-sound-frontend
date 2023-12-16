7c311a0a6ef1f5f217a84c7738d2bea3756f86ce
Rose van Leeuwen <i499833@fhict.nl>
2023-12-16 18:43:15 +0100

    can display userlist, its still buggy

--- src/app/shared/[id]/page.js
+++ src/app/shared/[id]/page.js
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState, useMemo } from 'react';
 import GridContainerBoard from '../../../../component/GridContainerBoard.client.js';
-import io from 'socket.io-client';
+import { io } from 'socket.io-client';
 // Create an audio element
 const audioElement = new Audio();
 
@@ -21,53 +21,70 @@ function playOrStopAudio(url) {
   }
 }
 
+
 export default function Page({ params }) {
   const [jsondata, setJson] = useState(null);
-  const urlid = params.id
+  const [userlist, setUserlist] = useState([]); // Use useState for userlist
+  const urlid = params.id;
+
+  let s = io(process.env.NEXT_PUBLIC_SOCKET)
+  s.on("userjoined", (ulist) => {
+    console.log("something")
+    setUserlist(ulist.split(","))
+    console.log("ulist: " + ulist)
+  })
+
+  s.on('executeCode', (url) => {
+
+    playOrStopAudio(url)
+  });
+
 
   const connectsocket = () => {
-    const socket = io(process.env.NEXT_PUBLIC_SOCKET)
-    socket.emit('joinroom', urlid)
-
-    socket.on('executeCode', (url) => {
-    
-      playOrStopAudio(url)
-    });
-    return socket
+    s.emit('joinroom', urlid, "user");
+    return s;
   };
-  const s = useMemo(() => connectsocket(), []);
 
+  useMemo(() => connectsocket(), []);
 
   useEffect(() => {
+    s.on("userjoined", (ulist) => {
+      console.log("something");
+      const updatedUserlist = ulist.split(',');
+      console.log("userlist: " + updatedUserlist);
+      setUserlist(updatedUserlist); // Update the state here
+    });
+
     const getBoard = async () => {
       try {
         const response = await fetch(`${process.env.NEXT_PUBLIC_AUDIO_API}/Boards/Session/${urlid}`);
         if (!response.ok) {
-          location
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
         setJson(data);
-
-
-        // const s = useMemo(() => connectsocket(),[]);
-
-
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
+
     getBoard();
   }, [urlid]);
 
-
-  if (!jsondata) {
-
-    return <div>Loading...</div>; // or a loading spinner, etc.
+  if (jsondata == null) {
+    return <div>Loading...</div>;
   }
 
   return (
     <div>
+      <div>
+        <h3>Connected Users:</h3>
+        <ul>
+          {userlist.map((user, index) => (
+            <li key={index}>{user}</li>
+          ))}
+        </ul>
+      </div>
       <GridContainerBoard json={jsondata} socket={s}></GridContainerBoard>
     </div>
   );
